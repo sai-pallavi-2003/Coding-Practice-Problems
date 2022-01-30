f1dcf2a3f55ae0950d967e821e455e1e25159236
SISTLA SAI PALLAVI <83531350+sai-pallavi-2003@users.noreply.github.com>
2022-01-30 19:38:59 +0530

    Create FindDiffPair.cpp

---
+++ Search And Sort/FindDiffPair.cpp
@@ -0,0 +1,53 @@
+/*Given an array Arr[] of size L and a number N, you need to write a program to find if there exists a pair of elements in the array whose difference is N.
+
+Example 1:
+
+Input:
+L = 6, N = 78
+arr[] = {5, 20, 3, 2, 5, 80}
+Output: 1
+Explanation: (2, 80) have difference of 78.*/
+
+
+
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+ 
+using namespace std; 
+
+
+bool findPair(int arr[], int size, int n);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int l,n;
+        cin>>l>>n;
+        int arr[l];
+        for(int i=0;i<l;i++)
+            cin>>arr[i];
+        if(findPair(arr, l, n))
+            cout<<1<<endl;
+        else cout<<"-1"<<endl;
+    }
+    
+  
+    return 0;
+}// } Driver Code Ends
+
+
+bool findPair(int arr[], int size, int n){
+    //code
+    for (int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            if(abs(arr[i]-arr[j])==n){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
